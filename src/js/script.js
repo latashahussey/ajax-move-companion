119cf9ef388a81f8bd093407c4f29505a14a9d67
sugarcookie <tasha@lhussey.com>
2017-03-27 21:38:54 -0500

    Load Google StreetView API

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,32 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
 
-    // load streetview
 
-    // YOUR CODE GOES HERE!
 
-    return false;
+  // load streetview
+  var $streetAddress = $('#street').val();
+  var $city = $('#city').val();
+  var $location = $streetAddress + ", " + $city;
+  var $src = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + $location;
+
+  $greeting.text("So you wanna live at " + $streetAddress + "?");
+
+  $('body').append("<img>")
+  $('img').addClass('bgimg').attr('src', $src);
+
+
+
+
+  return false;
 };
 
 $('#form-container').submit(loadData);
